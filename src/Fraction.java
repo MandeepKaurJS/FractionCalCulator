35531a706a4868d0e136522e02bd7d1b53a7c3f9
Mandeep Kaur <mandeep.kaur.fr@gmail.com>
2019-08-02 14:36:47 -0700

    PRogram for fraction Calculator

---
+++ src/Fraction.java
@@ -0,0 +1,89 @@
+import org.omg.CORBA.MARSHAL;
+
+import java.util.zip.Adler32;
+
+public class Fraction {
+    private int numerator;
+    private int denumenator;
+
+    public Fraction(int num, int denume) {
+        this.numerator=num;
+        this.denumenator=denume;
+    }
+    public Fraction(int num){
+        this.numerator=num;
+        this.denumenator=1;
+        this.Operation();
+    }
+    public Fraction(){
+        this.numerator=0;
+        this.denumenator=1;
+    }
+    public  void Operation(){
+        if(numerator<0 && denumenator<0){
+            this.numerator=Math.abs(numerator);
+            this.denumenator= Math.abs(denumenator);
+        }else if(numerator<0||denumenator<0){
+            this.numerator=-(Math.abs(numerator));
+            this.denumenator=Math.abs(denumenator);
+        }else if(this.denumenator==0){
+            throw new IllegalArgumentException("Demoniator can't be Zero!");
+        }
+    }
+    public int GetNumerator(){
+        return this.numerator;
+    }
+    public int GetDenominator(){
+        return this.denumenator;
+    }
+    public String toString(){
+        String fractionString;
+        if(this.denumenator==this.numerator){
+            fractionString=String.valueOf(1);
+        }else if(this.denumenator==1){
+            fractionString=String.valueOf(this.numerator);
+        }else {
+            fractionString=this.numerator+"\n"+this.denumenator;
+        }
+        return fractionString;
+    }
+    public double toDouble(){
+        double numDouble=(double)numerator;
+        double denDouble=(double)denumenator;
+        double result=numDouble/denDouble;
+        return result;
+    }
+    public Fraction add(Fraction addition){
+        int newNum=this.numerator* addition.denumenator+this.denumenator*this.numerator;
+        int newDen=this.denumenator*addition.denumenator;
+        Fraction newF=new Fraction(newNum,newDen);
+        return newF;
+    }
+
+    public Fraction subtract(Fraction minus){
+        int newNum=this.numerator* minus.denumenator-this.denumenator*this.numerator;
+        int newDen=this.denumenator*minus.denumenator;
+        Fraction newF=new Fraction(newNum,newDen);
+        newF.Operation();
+        return newF;
+    }
+    public Fraction Multiply(Fraction multiply){
+        int newNum=this.numerator* multiply.denumenator;
+        int newDen=this.denumenator*multiply.denumenator;
+        Fraction newF=new Fraction(newNum,newDen);
+        newF.Operation();
+        return newF;
+    }
+    public Fraction Divide(Fraction divide){
+        int newNum=this.numerator* divide.denumenator;
+        int newDen=this.denumenator*divide.denumenator;
+        Fraction newF=new Fraction(newNum,newDen);
+        return newF;
+    }
+    public void toLowerTerms(){
+
+    }
+    public int GCD(int num,int dem){
+        return  num;
+    }
+}
---
+++ src/FractionCalculator.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class FractionCalculator {
+    public static void main(String[] args){
+        System.out.println("This program is a fraction calculator");
+        System.out.println("It will add, subtract, multiply and divide fraction until you type Q to quit.");
+        System.out.println("Please enter your fractions in the form a/b, where a and b are integers.");
+        System.out.println("-----------------------------------------------------------------------------");
+        Scanner input=new Scanner(System.in);
+
+    }
+}
